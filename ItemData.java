47ab027c8b9e48faea91272e0a87323a260a636f
Bindu Anand <137068272+binduanand@users.noreply.github.com>
2024-12-10 19:38:47 +0530

    Create ItemData.java

---
+++ ItemData.java
@@ -0,0 +1,22 @@
+package com.project.datavisualizationproject;
+public class ItemData {
+    private String itemName;
+    private double itemValue;
+
+    // Constructor
+    public ItemData(String itemName, double itemValue) {
+        this.itemName = itemName;
+        this.itemValue = itemValue;
+    }
+
+    // Getters
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getItemValue() {
+        return itemValue;
+    }
+}
+
+
